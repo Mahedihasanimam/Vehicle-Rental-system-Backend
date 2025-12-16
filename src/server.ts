dbd8d8144d4b085c9b7731e8a541e2e08d1ee74e
Mahedihasanimam <mdmehedihasen678@gmail.com>
2025-12-16 10:41:58 +0600

    first commit- initial server setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
